c1e7263cd47e1da27c07ec5bed381f7d6bdc780e
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2024-01-09 11:45:33 -0300

    Blocking opponent's buttons and displaying the choice of the round.

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,33 +1,44 @@
-import { useDispatch, useSelector } from "react-redux"
-import Score from "../Score"
-import Joystick from "../Joystick"
-import { useEffect } from "react"
-import { setRoom } from "../../reducers/gameSlice"
+import { useDispatch, useSelector } from "react-redux";
+import Score from "../Score";
+import Joystick from "../Joystick";
+import { useEffect, useState } from "react";
+import { setRoom } from "../../reducers/gameSlice";
 
 export default function Game() {
-  const state = useSelector((state) => state.game)
-  const dispatch = useDispatch()
+  const state = useSelector((state) => state.game);
+  const [round, setRound] = useState();
+  const dispatch = useDispatch();
+  const isOwner = state?.room?.owner?.socketId === state?.socket?.id;
 
   useEffect(() => {
-    state?.socket?.on('roundPlayed', (data) => {
-      console.log('roundPlayed', data)
-      dispatch(setRoom(data))
-    })
-  }, [])
+    state?.socket?.on("roundPlayed", (data) => {
+      console.log("roundPlayed", data);
+      dispatch(setRoom(data));
+    });
+  }, []);
 
-  return (<>
-    <h2 className="text-center">Round: {state?.room?.rounds?.length}</h2>
-    <div className="d-flex justify-content-between">
-      <div className="alert alert-secondary text-center">
-        <h2 className="text-center">{state?.room?.owner?.nickname}</h2>
-        <Score score={state?.room?.players[0]?.score} />
-        <Joystick />
+  useEffect(() => {
+    if(state?.room?.rounds?.length > 1) {
+      setRound(state?.room?.rounds[state?.room?.rounds?.length - 1])
+    }
+    console.log('round', round)
+  }, [state?.room?.rounds]);
+
+  return (
+    <>
+      <h2 className="text-center">Round: {state?.room?.rounds?.length}</h2>
+      <div className="d-flex justify-content-between">
+        <div className="alert alert-secondary text-center">
+          <h2 className="text-center">{state?.room?.owner?.nickname}</h2>
+          <Score score={state?.room?.players[0]?.score} />
+          <Joystick disabled={!isOwner} choice={round && round?.p1.play} />
+        </div>
+        <div className="alert alert-secondary text-center">
+          <h2>{state?.room?.players[1]?.nickname}</h2>
+          <Score score={state?.room?.players[1]?.score} />
+          <Joystick disabled={isOwner} choice={round && round?.p2.play} />
+        </div>
       </div>
-      <div className="alert alert-secondary text-center">
-        <h2>{state?.room?.players[1]?.nickname}</h2>
-        <Score score={state?.room?.players[1]?.score} />
-        <Joystick />
-      </div>
-    </div>
-    </>)
+    </>
+  );
 }
--- src/components/Joystick/index.tsx
+++ src/components/Joystick/index.tsx
@@ -1,22 +1,32 @@
 import { useSelector } from "react-redux"
+import './Joystick.css'
 
 
-export default function Joystick() {
+export default function Joystick({ disabled, choice }) {
     const state = useSelector((state) => state.game)
+    const className = disabled ? 'btn btn-light btn-lg m-2 notAllowed' : 'btn btn-light btn-lg m-2'
+
+    const move = {
+        rock: '🪨',
+        paper: '📄',
+        scissors: '✂️'
+    }
 
     const play = (move: string) => () => {
         console.log(`Play ${move}`)
         state?.socket?.emit('play', {
             roomId: state?.room?.roomId,
             move,
-            socketId: state?.socket?.id
+            socketId: state?.socket?.id,
+            isOwner: state?.room?.owner?.socketId === state?.socket?.id
         })
     }
   return (
     <div>
-        <button onClick={play('rock')} className="btn btn-light btn-lg m-2">🪨</button>
-        <button onClick={play('paper')} className="btn btn-light btn-lg m-2">📄</button>
-        <button onClick={play('scissors')} className="btn btn-light btn-lg m-2">✂️</button>
+        <button onClick={play('rock')} disabled={disabled} className={className}>🪨</button>
+        <button onClick={play('paper')} disabled={disabled} className={className}>📄</button>
+        <button onClick={play('scissors')} disabled={disabled} className={className}>✂️</button>
+        <div className="choice">{move[choice]}</div>
     </div>
   )
 }
